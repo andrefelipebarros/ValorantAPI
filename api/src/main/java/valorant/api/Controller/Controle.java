f364569c52b93b10fd505381ac9e349235dd3f45
André Felipe de Barros A. N. <100530782+andrefelipebarros@users.noreply.github.com>
2024-04-15 01:16:01 -0300

    Update Controle.java

--- api/src/main/java/valorant/api/Controller/Controle.java
+++ api/src/main/java/valorant/api/Controller/Controle.java
@@ -3,16 +3,31 @@ package valorant.api.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import valorant.api.Model.Personagens;
 import valorant.api.Repository.RepositoryPersonagens;
 
+@RestController
 public class Controle {
     @Autowired
-    private RepositoryPersonagens resPersonagens;
+    private RepositoryPersonagens repPersonagens;
+
     @GetMapping("/Personagem")
     public List<Personagens> getPersonagem(){
-        return resPersonagens.findAll();
+        return repPersonagens.findAll();
+    }
+
+    @PostMapping("/Personagem")
+    public ResponseEntity<?> postPersonagem(@RequestBody Personagens personagem){
+        //Criando uma variável que armazena uma entidade(Personagem) 
+        //Sendo armazenada no repositório
+        Personagens savePerson = repPersonagens.save(personagem);
+        //Retorna se a Entidade(Personagem) foi salvo (OK) no Banco
+        return ResponseEntity.ok(savePerson);
     }
 }
